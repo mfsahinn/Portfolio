d4c14f290ec329fc804907de263b7c7a4533f95b
Mustafa Fatih Şahin <m.fatihsahin97@gmail.com>
2024-03-23 01:42:23 +0300

    theme changed.

--- Portfolio.Core/obj/Debug/net8.0/Portfolio.Core.AssemblyInfo.cs
+++ Portfolio.Core/obj/Debug/net8.0/Portfolio.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+427a6984d6680aa5d183cdcbbc9c8fca1516b256")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0d30fbe3830d7ca06a9309a075cb1ead609bca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
